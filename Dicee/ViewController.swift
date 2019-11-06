ad2ba4a6df71d782ed0e51b7179524e914f7e987
Muthu <s.muthukhumar@gmail.com>
2019-11-06 21:57:02 +0800

    Initial commot

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Muthukumar on 06/11/2019.
+//  Copyright © 2019 Muthu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+   
+    @IBAction func btnRollPressed(_ sender: UIButton) {
+    
+        //change the button
+        leftDiceImageView.image = #imageLiteral(resourceName: "DiceSix")
+        rightDiceImageView.image = #imageLiteral(resourceName: "DiceFour")
+    
+    }
+
+}
+
